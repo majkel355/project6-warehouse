333fad6d43d20e9eb50c515425716e6e4385d154
majkel355 <129754260+majkel355@users.noreply.github.com>
2026-05-14 16:06:45 +0200

    transitions between scenes
    the ability to start the game by clicking the "start" button and the ability to exit the game by pressing the "exit" button

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
+
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
@@ -18,7 +21,20 @@ public class MenuUIHandler : MonoBehaviour
     private void Start()
     {
         ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        // this will call the NewColorSelected function when the color picker has a color button clicked
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+    #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+    #else
+            Application.Quit(); // original code to quit Unity player
+    #endif
     }
 }
